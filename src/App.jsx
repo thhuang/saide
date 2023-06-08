b591041905a2bde1068dc33bab91cbe89da054ed
TH Huang <tzhsn.huang@gmail.com>
2023-06-08 16:55:56 -0500

    added react-query and router

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,19 @@
-import { useRef } from 'react';
-import Editor from '@monaco-editor/react';
-import * as Y from 'yjs';
-import { WebsocketProvider } from 'y-websocket';
-import { MonacoBinding } from 'y-monaco';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import Editor from './components/Editor';
+
+const queryClient = new QueryClient();
 
 function App() {
-  const editorRef = useRef(null);
-
-  function editorOnMount(editor) {
-    editorRef.current = editor;
-
-    const doc = new Y.Doc();
-    // const provider = new WebsocketProvider('wss://demos.yjs.dev', 'monaco-demo', doc);
-    const provider = new WebsocketProvider('ws://127.0.0.1:13701', 'monaco-demo', doc);
-    const type = doc.getText('monaco');
-
-    new MonacoBinding(
-      type,
-      editorRef.current.getModel(),
-      new Set([editorRef.current]),
-      provider.awareness
-    );
-  }
-
   return (
-    <Editor
-      height="100vh"
-      width="100vw"
-      theme="vs-dark"
-      defaultLanguage="cpp"
-      onMount={editorOnMount}
-    />
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter basename="/saide">
+        <Routes>
+          <Route path="/" element={<Editor />} />
+        </Routes>
+      </BrowserRouter>
+    </QueryClientProvider>
   );
 }
 
