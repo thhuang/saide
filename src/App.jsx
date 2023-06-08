b57dde80ad1f6b4f705957354e6cb548105b73f3
TH Huang <tzhsn.huang@gmail.com>
2023-06-08 18:28:34 -0500

    added landing page

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 import Editor from './components/Editor';
 
@@ -10,10 +11,31 @@ function App() {
     <QueryClientProvider client={queryClient}>
       <BrowserRouter basename="/saide">
         <Routes>
-          <Route path="/" element={<Editor />} />
+          <Route path="/" element={<Landing />} />
+          <Route path="/editor" element={<Editor />} />
         </Routes>
       </BrowserRouter>
     </QueryClientProvider>
+  );
+}
+
+function Landing() {
+  const navigate = useNavigate();
+
+  return (
+    <div className="h-screen flex flex-col justify-center gap-y-8 place-items-center">
+      <h1 className="font-sans font-semibold text-5xl bold mb-10">
+        Super Awesome IDE
+      </h1>
+      <div className="flex flex-row gap-x-2">
+        <a className="btn btn-primary" onClick={() => navigate('/editor')}>
+          Login
+        </a>
+        <a className="btn" onClick={() => navigate('/editor')}>
+          Guest
+        </a>
+      </div>
+    </div>
   );
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,16 @@ export default {
   mode: 'jit',
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        white: '#f8f8ff',
+        black: '#1f2335',
+        blue: '#22b7eb',
+        grey: '#545c7e',
+        background: '#1a1b26',
+        'background-darker': '#16161e',
+      },
+    },
   },
   plugins: [],
 };
