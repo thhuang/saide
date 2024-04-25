6c346a701243b9cfaed1ba6be5a3e6d9b0909f47
SammiH2022 <96802164+SammiH2022@users.noreply.github.com>
2024-04-24 23:38:17 -0500

    update a tags to Link tags

--- app/[[...slug]]/client.tsx
+++ app/[[...slug]]/client.tsx
@@ -2,29 +2,30 @@
 
 import React from 'react'
 import dynamic from 'next/dynamic'
+import Link from 'next/link'
 
-const App = dynamic(() => import('../../App'), { ssr: false })
+// const App = dynamic(() => import('../../App'), { ssr: false })
 
 export function ClientOnly() {
-      return (
+    return (
         <div className="h-screen flex flex-col justify-center gap-y-8 place-items-center">
-          <h1 className="font-sans font-semibold text-5xl bold mb-10">
-            Super Awesome IDE
-          </h1>
-          <div className="flex flex-row gap-x-2">
-            <a
-              className="btn btn-primary"
-              // href="http://localhost:8080/oauth2/authorization/github"
-            //   onClick={() => navigate('/main')}
-            >
-              Login
-            </a>
-            <a className="btn"
-            // onClick={() => navigate('/editor')}
-            >
-              Guest
-            </a>
-          </div>
+            <h1 className="font-sans font-semibold text-5xl bold mb-10">
+                Super Awesome IDE
+            </h1>
+            <div className="flex flex-row gap-x-2">
+                <Link
+                    className="btn btn-primary"
+                    href="#"
+                >
+                    Login
+                </Link>
+                <Link
+                    className="btn"
+                    href="#"
+                >
+                    Guest
+                </Link>
+            </div>
         </div>
-      );
+    );
 }
--- app/[[...slug]]/page.tsx
+++ app/[[...slug]]/page.tsx
@@ -1,4 +1,3 @@
-// import '../../index.css'
 import React from 'react'
 import { ClientOnly } from './client'
 
